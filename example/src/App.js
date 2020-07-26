82f79515572bebf8b02d64b3b4de61398193edda
Natalie Smith <thatgalnatalie@gmail.com>
2020-07-25 17:58:07 -0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'shape-library'
+import 'shape-library/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
